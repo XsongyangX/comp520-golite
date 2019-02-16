69654bdfe395094c898f8d5e750ac78137e45519
Song Yang <songyang1218@gmail.com>
2019-02-15 22:23:51 -0500

    Add spinning bar program

---
+++ programs/1-scan+parse/valid/spinningBar.go
@@ -0,0 +1,30 @@
+/* This program will print a spinning bar. It will terminate after
+100 iterations. */
+
+package main
+
+func main() {
+	spinningBar()
+}
+
+func spinningBar() {
+
+	var i = 0
+	var j = 0
+	var state [4]rune
+	state[0] = '/'
+	state[1] = '-'
+	state[2] = '\\'
+	state[3] = '|'
+
+	for true {
+		println(string(state[i]))
+		i++
+		i %= 4
+		j++
+
+		if j == 100 {
+			break
+		}
+	}
+}
