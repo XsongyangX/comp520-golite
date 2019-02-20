e71da9afb483b6f41dda767d990356bfc7c62b10
Song YANG <syang68@ds2.CS.McGill.CA>
2019-02-19 21:15:48 -0500

    Downgrade constructs from Go to GoLite

--- programs/1-scan+parse/valid/quadratic.go
+++ programs/1-scan+parse/valid/quadratic.go
@@ -5,17 +5,17 @@ func sqrt(x float64) float64 {
 	var ans float64 = 1.0
 
 	for i := 0; i < 32; i++ {
-		ans = (ans + x/ans) / 2
+		ans = (ans + x/ans) / 2.0
 	}
 
 	return ans
 }
 
 func quadraticRoots(a float64, b float64, c float64) [2]float64 {
-	var discriminant = b*b - 4*a*c
+	var discriminant = b*b - 4.0*a*c
 
 	// negative discriminant
-	if discriminant < 0 {
+	if discriminant < 0.0 {
 		var error [2]float64
 		error[0] = 0.0
 		error[1] = 0.0
@@ -25,12 +25,14 @@ func quadraticRoots(a float64, b float64, c float64) [2]float64 {
 	discriminant = sqrt(discriminant)
 
 	var roots [2]float64
-	roots[0] = (-b + discriminant) / (2 * a)
-	roots[1] = (-b - discriminant) / (2 * a)
+	roots[0] = (-b + discriminant) / (2.0 * a)
+	roots[1] = (-b - discriminant) / (2.0 * a)
 	return roots
 }
 
 func main() {
 	println("The roots of x^2+2x-10 are:")
-	println(quadraticRoots(1, 2, -10))
+	var answer = quadraticRoots(1.0,2.0,-10.0)
+	println(answer[0])
+	println(answer[1])
 }
