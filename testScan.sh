6442f934111bae4275e7db23ef70fe522ecf7a08
Song YANG <syang68@ds2.CS.McGill.CA>
2019-02-21 15:49:12 -0500

    Add a test script for the scanner exclusively and make the run.sh linux-runnable

---
+++ testScan.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Tests the scanner exclusively
+# add tokens, if you want to print the tokens
+
+# build the scanner
+make clean -C ./src
+make scan -C ./src
+
+# run the scanner
+for file in programs/1-scan+parse/valid/*.go
+do
+	echo $file
+
+	if [[ $1 != "tokens" ]]
+	then
+		./run.sh scan $file
+	else
+		./run.sh tokens $file
+	fi
+done
+
+
+# run the tokenizer if specified
