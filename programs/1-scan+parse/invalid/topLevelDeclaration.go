9934cf45d610a10ef6b9e7c8f368050249154953
Song YANG <syang68@ds2.CS.McGill.CA>
2019-03-01 17:47:34 -0500

    Initial invalid top level declarations

---
+++ programs/1-scan+parse/invalid/topLevelDeclaration.go
@@ -0,0 +1,10 @@
+/* Invalid top level declaration: a for loop in the global scope */
+package main
+
+for i:= 0; i < 10; i++{
+	print(1)
+}
+
+func main(){
+
+}
---
+++ programs/1-scan+parse/invalid/topLevelDeclaration2.go
@@ -0,0 +1,9 @@
+/* Invalid top level declaration: block statement in global scope*/
+package main
+
+{
+	print(2)
+	println(3)
+}
+
+var s string
