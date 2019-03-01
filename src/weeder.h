bacfdc40906aed6fe3bb4f73dca3ac4bfa67e6b9
Song YANG <syang68@ds2.CS.McGill.CA>
2019-03-01 17:15:38 -0500

    Bare minimum of signatures

--- src/weeder.h
+++ src/weeder.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include "tree.h"
 
-void weedRoot(PROGRAM root);
+void weedRoot(PROGRAM *root);
+void weedProgram(PROGRAM *program);
+void weedDeclaration(DECLARATION *declaration);
+void weedFunction(FUNCTION *function);
+void weedStatement(STATEMENT *statement);
 
 #endif
