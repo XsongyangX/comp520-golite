7aa2e8e490c4b689e997b177ec382d5baa127ade
Song YANG <syang68@ds2.CS.McGill.CA>
2019-03-02 10:17:00 -0500

    Initial valid if statement with fancy variations

---
+++ programs/1-scan+parse/valid/if.go
@@ -0,0 +1,17 @@
+/* Valid fancy if statements */
+package main
+
+func fib(n int) int{
+	return 1
+}
+
+func main() {
+	var x int
+	if i:=0; x<i {
+		print(x)
+	} else if k:=i; x<k {
+		print(x)
+	} else if l:=fib(10); true {
+		print(x)
+	}
+}
