9206fba518ce14c4e03fc5b1116f70c72da6bbc2
Gregory Theos <gdtheos@gmail.com>
2019-03-02 16:15:37 -0500

    Bugfixing randInt.go
    bugfixes

--- programs/1-scan+parse/valid/randInt.go
+++ programs/1-scan+parse/valid/randInt.go
@@ -2,6 +2,12 @@ package main
 
 var seed int = 5;
 
+func rand() int{
+	var m, a, c int = 2^(32), 1664525, 1013904223;
+	seed++;
+	return (a*seed + c) % m;
+}
+
 func main() {
 	var randInt int;
 	randInt = rand();
@@ -10,10 +16,4 @@ func main() {
 	println(randInt);
 	randInt = rand();
 	println(randInt);
-}
-
-func rand() int{
-	var m, a, c int = 2^(32), 1664525, 1013904223;
-	seed++;
-	return (a*seed + c) % m;
 }
