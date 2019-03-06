d92da6713b8dc4407e5c5217ef0ee2745b1895a1
Song YANG <songyang1218@gmail.com>
2019-03-05 22:24:36 -0500

    Simplify python script with more high-level constructs

--- prettyChecker.py
+++ prettyChecker.py
@@ -16,22 +16,20 @@ def eprint(*args, **kwargs):
 
 def main():
 
-	# command line args
-	# argv[1] = go file 1
-	# argv[2] = go file 2
-	if len(sys.argv) != 3:
-		raise Exception("Run script like this: prettyChecker.py file1 file2")
+    # command line args
+    # argv[1] = go file 1
+    # argv[2] = go file 2
+    if len(sys.argv) != 3:
+        raise Exception("Run script like this: prettyChecker.py file1 file2")
 	
-	# open files
-	file1 = open(sys.argv[1], "r").readlines()
-	file2 = open(sys.argv[2], "r").readlines()
-	
-	for i in range(min(len(file1), len(file2))):
-		if file1[i] != file2[i]:
-			eprint("Files " + sys.argv[1] + " and " + sys.argv[2] + " differ on line " + str(i+1))
-			sys.exit(1)
+    # open files
+    with open(sys.argv[1], 'r') as file1, open(sys.argv[2], 'r') as file2:
+        for i, (line1, line2) in enumerate(zip(file1,file2), 1):
+            if line1 != line2:
+                eprint("Files " + sys.argv[1] + " and " + sys.argv[2] + " differ on line " + str(i))
+                sys.exit(1)
 
-	return
+    return
 
 if __name__ == "__main__":
-	main()
+    main()
