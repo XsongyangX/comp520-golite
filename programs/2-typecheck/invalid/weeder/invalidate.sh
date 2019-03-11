3f1802524c49297274e7cb478d00d319b8989fb7
Song YANG <songyang1218@gmail.com>
2019-03-10 20:25:52 -0400

    Change echo to cat

--- programs/2-typecheck/invalid/weeder/invalidate.sh
+++ programs/2-typecheck/invalid/weeder/invalidate.sh
@@ -2,9 +2,9 @@
 
 for file in *.go
 do
-	echo $file | ~cs520/golitec typecheck
-	if [[ $? != 0 ]]
+	cat $file | ~cs520/golitec typecheck
+	if [[ $? == 0 ]]
 	then
-		echo -e "\033[0;31m[Error]\033[0m: $file is invalid"
+		echo -e "\033[0;31m[Error]\033[0m: $file is valid"
 	fi
 done
