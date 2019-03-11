df133b88bef7403f46767fd59674d12c7eeb708e
Song Yang <song.yang3@mail.mcgill.ca>
2019-03-11 17:31:48 -0400

    Initial non-terminating switch statements

---
+++ programs/1-scan+parse/invalid/weeder/forLoop2.go
@@ -0,0 +1,10 @@
+/* An usual for-loop with a break inside is not a terminating statement */
+package main
+
+func main(){}
+
+func foo(x float64) float64 {
+	for ; x > 0.0; x = x + 1.0{
+		break
+	}
+}
---
+++ programs/1-scan+parse/invalid/weeder/switch1.go
@@ -0,0 +1,11 @@
+/* A switch statement without a default case is not terminating */
+package main
+
+func main(){}
+
+func foo(x float64) float64 {
+	switch x {
+	case x > 0: return 1.0
+	case x < 0: return 0.0
+	}
+}
