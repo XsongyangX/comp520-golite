94edfbe561e2241f798705ee178ba040f9126ef5
Song Yang <song.yang3@mail.mcgill.ca>
2019-03-11 17:42:08 -0400

    Add final non-terminating switch statements

---
+++ programs/1-scan+parse/invalid/weeder/switch4.go
@@ -0,0 +1,14 @@
+/* A switch statement with breaks but non-terminating statement lists 
+is not terminating */
+package main
+
+func main(){}
+
+func foo(x float64) float64 {
+	switch {
+	case x > 0.0: 
+	case x < 0.0: return 0.0
+	default:
+		break
+		return 1.0
+}
