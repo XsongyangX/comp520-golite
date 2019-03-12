b3ad33a224735a0821af70873f88f659292ce5b7
Song YANG <songyang1218@gmail.com>
2019-03-12 15:50:19 -0400

    Initial generate bash script

---
+++ programs/2-typecheck/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# look for valid/invalid directories
+if [ ! -d valid ]
+then
+	mkdir valid
+fi
+if [ ! -d invalid ]
+then
+	mkdir invalid
+fi
+
+# generate test programs
+python3 scope.py generate
