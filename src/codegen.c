32a9a80373ef813f016bdda3247205c55ae18cd3
charlesgingras <chging@hotmail.com>
2019-03-20 10:20:48 -0400

    Fixed some ptr typos and added some tips.

--- src/codegen.c
+++ src/codegen.c
@@ -12,24 +12,24 @@ void codegenStatement(STATEMENT *s){
 	
 }
 
-void codegenExpression(EXP *e, SYMTABLE *table){
+void codegenExpression(EXP *e, symTable *table){
 	switch (e->kind){
 		case emptyExp:
 			break;
 		case intExp:
-			printf("%d", e->val->intLiteral);
+			printf("%d", e->val.intLiteral);
 			break; 
 		case floatExp:
-			printf("%f", e->val->floatLiteral);
+			printf("%f", e->val.floatLiteral);
 			break;  
 		case strExp: 
-			printf("\"%s\"", e->val->strLiteral);
+			printf("\"%s\"", e->val.strLiteral);
 			break; 
 		case rawstrExp:
-			printf("\"%s\"", e->val->strLiteral);
+			printf("\"%s\"", e->val.strLiteral);
 			break; 
 		case boolExp: 
-			if (e->val->intLiteral == 1){
+			if (e->val.intLiteral != 0){
 				printf("True");
 			}
 			else{
@@ -37,129 +37,133 @@ void codegenExpression(EXP *e, SYMTABLE *table){
 			}
 			break;
 		case runeExp:
-			printf("\'%c\'", e->val->runeLiteral);
+			printf("\'%c\'", e->val.runeLiteral);
 			break;
 		case plusExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("+");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case minusExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("-");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case timesExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("*");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case divExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("/");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case modExp:
-			codegenExpression(e->val->binary->lhs);
-			printf("\%");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.lhs, table);
+			printf("%%");
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case notExp:
 			printf("!");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case posExp:
 			/*We should handle non-int types in here IIRC
 			I just can't think of a way to do it neatly at the moment*/
 			printf("abs(");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			printf(")");
 			break;
 		case negExp:
 			printf("-");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case parExp:
 			printf("(");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			printf(")");
 			break;
 		case eqExp:
-			if (e->val->binary->lhs->kind == idExp){
-				SYMBOL *tmpSymb1 = getSymbol(table, e->val->binary->lhs->val->identifier, structSymbol)
+			/*TODO: undeclared names, note that you cannot declare a 
+			variable inside a case stmt. You should do so before the switch.*/
+			if (e->val.binary.lhs->kind == idExp){
+				SYMBOL *tmpSymb1 = getSymbol(table, e->val.binary.lhs->val.identifier, structSymbol);
 				if (tmpStruct != NULL){
-					SYMBOL *tmpSymb2 = getSymbol(table, e->val->binary->rhs->val->identifier, structSymbol)
+					SYMBOL *tmpSymb2 = getSymbol(table, e->val.binary.rhs->val.identifier, structSymbol);
 					eqExpStructs(tmpSymbol1, tmpSymbol2, 1, "");
 				}
 				else{
-					codegenExpression(e->val->lhs);
+					codegenExpression(e->val.binary.lhs, table);
 					printf("==");
-					codegenExpression(e->val->rhs);
+					codegenExpression(e->val.binary.rhs, table);
 				}
 			}
 			else{
-				codegenExpression(e->val->lhs);
+				codegenExpression(e->val.binary.lhs, table);
 				printf("==");
-				codegenExpression(e->val->rhs);
+				codegenExpression(e->val.binary.rhs, table);
 			}
 			break;
 		case neqExp:
-			if (e->val->binary->lhs->kind == idExp){
-				SYMBOL *tmpSymb1 = getSymbol(table, e->val->binary->lhs->val->identifier, structSymbol)
+			/*TODO: undeclared names, note that you cannot declare a 
+			variable inside a case stmt. You should do so before the switch.*/
+			if (e->val.binary.lhs->kind == idExp){
+				SYMBOL *tmpSymb1 = getSymbol(table, e->val.binary.lhs->val.identifier, structSymbol);
 				if (tmpStruct != NULL){
-					SYMBOL *tmpSymb2 = getSymbol(table, e->val->binary->rhs->val->identifier, structSymbol)
+					SYMBOL *tmpSymb2 = getSymbol(table, e->val.binary.rhs->val.identifier, structSymbol);
 					eqExpStructs(tmpSymbol1, tmpSymbol2, 0, "");
 				}
 				else{
-					codegenExpression(e->val->lhs);
+					codegenExpression(e->val.binary.lhs, table);
 					printf("!=");
-					codegenExpression(e->val->rhs);
+					codegenExpression(e->val.binary.rhs, table);
 				}
 			}
 			else{
-				codegenExpression(e->val->lhs);
+				codegenExpression(e->val.binary.lhs, table);
 				printf("!=");
-				codegenExpression(e->val->rhs);
+				codegenExpression(e->val.binary.rhs, table);
 			}
 			break;
 		case geqExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf(">=");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case leqExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("<=");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case gtExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf(">");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case ltExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("<");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case orExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("||");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case andExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("&&");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case andnotExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("& -(");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			printf(")");
 			break;
 		case idExp:
-			printf("%s", e->val->identifier);
+			printf("%s", e->val.identifier);
 			break;
 		/*
 		-------------------------
@@ -169,43 +173,43 @@ void codegenExpression(EXP *e, SYMTABLE *table){
 		case expblockExp:
 		*/
 		case bitAndExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("&");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case bitOrExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("|");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case xorExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("^");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case lshiftExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("<<");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case rshiftExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf(">>");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case indexExp:
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		case elementExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf("[");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			printf("]");
 			break;
 		case invocExp:
-			codegenExpression(e->val->binary->lhs);
+			codegenExpression(e->val.binary.lhs, table);
 			printf(".");
-			codegenExpression(e->val->binary->rhs);
+			codegenExpression(e->val.binary.rhs, table);
 			break;
 		/*
 		-------------------------
@@ -218,8 +222,8 @@ void codegenExpression(EXP *e, SYMTABLE *table){
 		*/
 		case uxorExp:
 			//As far as I can tell, 1111111... in C
-			printf("2147483647^(")
-			codegenExpression(e->val->binary->rhs);
+			printf("2147483647^(");
+			codegenExpression(e->val.binary.rhs, table);
 			printf(")");
 		case funcExp:
 		default:
@@ -236,10 +240,10 @@ void codegenExpression(EXP *e, SYMTABLE *table){
 //e.g. struct.innerStruct.innerInnerStruct and the like
 void eqExpStructs(SYMBOL *s1, SYMBOL *s2, int mode, char *nameSoFar){
 	printf("(");
-	SYMBOL *field1 = s1->val->structFields;
-	SYMBOL *field2 = s2->val->structFields;
+	SYMBOL *field1 = s1->val.structFields;
+	SYMBOL *field2 = s2->val.structFields;
 	while(field1 != NULL){
-		if(*field1->kind != structSym){
+		if(field1->kind != structSym){
 			printf("%s%s.%s", nameSoFar, s1->name, field1->name);
 			if (mode){
 				printf("==");
