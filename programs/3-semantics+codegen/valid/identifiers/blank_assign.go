2c94bcf121fb7bacd4d33a11745d484b064c63eb
Song Yang <song.yang3@mail.mcgill.ca>
2019-03-25 16:50:25 -0400

    Blank identifier semantic test files

---
+++ programs/3-semantics+codegen/valid/identifiers/blank_assign.go
@@ -0,0 +1,12 @@
+//~hi
+
+package main
+
+func main() {
+	var _ = foo()
+}
+
+func foo() int {
+	print("hi")
+	return 0
+}
---
+++ programs/3-semantics+codegen/valid/identifiers/blank_param.go
@@ -0,0 +1,11 @@
+//~hi
+
+package main
+
+func main() {
+	foo(1, 1)
+}
+
+func foo(_ int, _ int) {
+	print("hi")
+}
