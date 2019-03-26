d8ae9cc76ae51ae6c6522e457f80a29ab57039a5
Song YANG <songyang1218@gmail.com>
2019-03-26 15:41:25 -0400

    Initialization semantics test file

---
+++ programs/3-semantics+codegen/valid/var_declaration/initialize.go
@@ -0,0 +1,45 @@
+//~false
+//~0
+//~+0.000000e+000
+//~
+//~0
+//~false
+//~0
+//~
+//~0
+package main
+
+func main() {
+	
+	// initialization of base types
+	var b bool // false
+	println(b)
+
+	var n int // 0
+	println(n)
+
+	var x float64
+	println(x) // +0.000000e+000
+
+	var s string
+	println(s) // "" (when printed, it shows nothing)
+
+	var r rune
+	println(r) // 0
+	
+	// structs
+	var x struct { m bool; }
+	println(x.m) // false
+
+	var ooo struct{
+		x int
+		s struct {
+			k string
+			l rune
+		}
+	}
+	println(ooo.x) // 0
+	println(ooo.s.k) // ""
+	println(ooo.s.l) // 0
+
+}
