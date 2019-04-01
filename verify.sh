55142d956d85e1275bc7eb16a525c46d7512b95b
Song YANG <songyang1218@gmail.com>
2019-03-31 23:36:24 -0400

    Conditional compiling

--- verify.sh
+++ verify.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # run test.sh to codegen first
-#./test.sh
+if [[ ! -f programs/3-semantics+codegen/valid/identifiers/blank_param.c ]]
+then
+	./test.sh
+fi
 
 # call verify
 for folder in programs/3-semantics+codegen/valid/*/
 do
-	for file in $folder/*.go
+	for file in $folder*.go
 	do
 		echo "##### $file  #####"
 		programs/3-semantics+codegen/valid/verify.sh $file
