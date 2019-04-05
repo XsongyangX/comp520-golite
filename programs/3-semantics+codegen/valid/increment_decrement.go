df8d0909c7a1f5694ab534fcd2c05bf499b8461d
Song YANG <songyang1218@gmail.com>
2019-04-05 12:28:45 -0400

    Increment/decrement test files

---
+++ programs/3-semantics+codegen/valid/increment_decrement.go
@@ -0,0 +1,52 @@
+//~1
+//~3.000000e+00
+//~98
+//~-2
+//~-1.353000e+01
+//~62
+//~in foo
+//~1
+package main
+
+func foo() int {
+	println("in foo")
+	return 0
+}
+
+func main() {
+
+	// increment
+	n := 0
+	n++
+	println(n)
+
+	x := 2.0
+	x++
+	println(x)
+
+	r := 'a'
+	r++
+	println(r)
+
+	// decrement
+	m := -1
+	m--
+	println(m)
+
+	y := -12.530
+	y--
+	println(y)
+
+	s := '?'
+	s--
+	println(s)
+
+	// advanced
+	var a [1]int
+	a[foo()]++ // call foo once -> "in foo", a[0] = 1
+	println(a[0])
+
+
+
+}
+
