79267759e1558069d3e831e590ffc3acf1ae7df8
Song YANG <songyang1218@gmail.com>
2019-04-05 13:56:53 -0400

    More for loop examples

--- programs/3-semantics+codegen/valid/for.go
+++ programs/3-semantics+codegen/valid/for.go
@@ -1,6 +1,12 @@
 //~in loop
 //~012345678910111213141516171819
 //~the second loop
+//~hi
+//~hihi
+//~hi
+//~hihi
+//~hi
+//~0102030405060
 package main
 
 func main() {
@@ -26,6 +32,22 @@ func main() {
 		}
 
 		println("after continue")
+	}
+
+	for x, y, z := 0, 1, 2; x+y+z < 10; x, y, z = y+1, z+1, x+1 {
+		println("hi")
+		if x+y+z == 9 {
+			continue
+		}
+		println("hihi")
+	}
+
+	var i = 0
+	for ; i < 100; i+=10 {
+		print(i)
+		if i > 50 {
+			break
+		}
 	}
 
 }
