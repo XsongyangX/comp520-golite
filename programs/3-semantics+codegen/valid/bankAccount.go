96f317f8036f9bbab17828e33ddce780452a6ab5
Song YANG <songyang1218@gmail.com>
2019-04-07 13:48:38 -0400

    Some milestone 1 programs with annotations now in semantics test set

---
+++ programs/3-semantics+codegen/valid/bankAccount.go
@@ -0,0 +1,36 @@
+//~New balance:+1.000000e+02
+package main
+
+type Account struct {balance float64; password string;}
+
+func deposit(pass string, amount float64, acct Account){
+	if acct.password == pass {
+		acct.balance += amount;
+		print("New balance:");
+		print(acct.balance);
+	}else{
+		print("Wrong password");
+	}
+}
+
+func withdraw(pass string, amount float64, acct Account){
+	if acct.password == pass {
+		if acct.balance >= amount {
+			acct.balance -= amount;
+			print("New balance:");
+			print(acct.balance);
+		}else {
+			print("Not enough funds");
+		}
+
+	}else {
+		print("Wrong password");
+	}
+}
+func main(){
+
+	var acct1 Account;
+	acct1.balance = 0.0;
+	acct1.password = "foo";
+	deposit("foo", 100.00, acct1);
+}
