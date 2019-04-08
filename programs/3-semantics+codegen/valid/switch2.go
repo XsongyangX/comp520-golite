d59e5be571a8ed75f3d106e967a9dc5a6ac6e0c3
Song YANG <songyang1218@gmail.com>
2019-04-07 23:45:32 -0400

    More switch test file

---
+++ programs/3-semantics+codegen/valid/switch2.go
@@ -0,0 +1,31 @@
+//~1100
+/* Example of switch statements */
+package main
+
+func getMyNumber() int {
+
+	return 100
+}
+
+func main () {
+	var x int
+
+	// numerical cases
+	switch x {
+		default: print(x)
+		case 0, 1, 2, 3: x++
+		case 4,5,6,7: x--
+	}
+
+	// missing switch expression
+	switch {
+		case x == 1: print(1)
+	}
+
+	// short declaration and missing switch expression
+	switch x = getMyNumber(); {
+		case x<0: print(x)
+		default: print(x)
+	}
+}
+
